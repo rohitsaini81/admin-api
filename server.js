93d97f5b1842f27ce1ac74f1cbce60b12bab639b
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2023-09-08 17:38:30 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ const uri= "mongodb+srv://"+process.env.URI_PASS+"@cluster0.8t0hk4y.mongodb.net/
 app.use(cors())
 app.use(bodyParser.json());
 
-
 // Connect to the MongoDB database
-const dbcon=async(uri)=>{await mongoose.connect(uri, {
+const dbcon=(uri)=>{ mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(console.log("db connected !")).catch(e=>{console.log(e)})}
 // Define API routes
+
 const bookRoutes = require('./routes/pRoutes');
 app.use('/api/', bookRoutes);
 dbcon(uri);
