2b39abffb8b4f00f3079eae4753b5a96ff63afa2
Rohit Saini <70269264+rohitsaini81@users.noreply.github.com>
2023-09-09 15:34:17 +0530

    Update projects.js

--- models/projects.js
+++ models/projects.js
@@ -5,7 +5,10 @@ const moviesSchema = new mongoose.Schema({
   decription: { type: String},
   link: { type: String },
   prolink: { type: String },
-
+  text1: { type: String },
+  text2: { type: String },
+  text3: { type: String },
+  text4: { type: String },
 
 });
 
